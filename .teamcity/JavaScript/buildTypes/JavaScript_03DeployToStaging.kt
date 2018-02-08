65a34c5cfbd85a79403c222c9ce90198be53b3a2
AdewaleOdunlami <adewaleodunlami@yahoo.com>
2018-02-08 07:41:37 -0500

    updated script

--- .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
+++ .teamcity/JavaScript/buildTypes/JavaScript_03DeployToStaging.kt
@@ -19,7 +19,8 @@ object JavaScript_03DeployToStaging : BuildType({
             name = "IIS Deploy"
             id = "RUNNER_5"
             scriptContent = """
-                xcopy /S /I /Y /R "C:\BuildAgent\work\1dd1c8ec653ef30a\app" "C:\inetpub\wwwroot"
+                rmdir /S /Q inetpub\wwwroot
+                xcopy /S /I /Y /R app inetpub\wwwroot
                 """
         }
     }
